fc3b82a92d02b44ffd163b7548428012d87f11a3
decker <mdatmosci@gmail.com>
2024-11-11 13:49:15 -0600

    changed checking function to return the string ref after we check it we do not need the option type, read in the contents of a file.  need to check file exists next

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,27 @@
 use std::env;
+use std::fs;
 
-fn check_args(v: Option<&String>)  {
+fn check_args(v: Option<&String>) -> &String  {
     match v {
-        Some(v) => println!("Found argument {v}"),
+        Some(v) => println!(""),
         None => panic!("minigrep requires both search string and file path as arguments"),
     }
+    v.unwrap()  //return the unwrapped String ref
 }
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    //println!("args length is {}",args.len());
-    //if args.len() < 3 {
-    //    panic!("minigrep requires the search query and file to be specified");
-    //}
-    //we should not hard code with [1] to prevent runtime error
-    //let query = &args[1];
-    //let file_path = &args[2];
-
     let query: Option<&String> = args.get(1);
-    check_args(query);
-    //match query {
-    //    Some(query) => println!("Query is {query}"),
-    //    None => panic!("minigrep requires a query"),
-    //}
+    let checked_query = check_args(query);
 
     let file_path: Option<&String> = args.get(2);
-    check_args(file_path);
-    //match file_path {
-    //    Some(file_path) => println!("Filepath is {file_path}"),
-    //    None => panic!("minigrep requires a filepath"),
-    //}
+    let checked_file_path = check_args(file_path);
 
+    let file_contents = fs::read_to_string(checked_file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{file_contents}");
     dbg!(args);
 }
