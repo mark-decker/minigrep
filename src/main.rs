88a809e53d60209760341348244af8f10b95ff6e
decker <mdatmosci@gmail.com>
2024-11-11 13:27:28 -0600

    moved checking we get valid input to a function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use std::env;
 
+fn check_args(v: Option<&String>)  {
+    match v {
+        Some(v) => println!("Found argument {v}"),
+        None => panic!("minigrep requires both search string and file path as arguments"),
+    }
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -12,16 +20,18 @@ fn main() {
     //let file_path = &args[2];
 
     let query: Option<&String> = args.get(1);
-    match query {
-        Some(query) => println!("Query is {query}"),
-        None => panic!("minigrep requires a query"),
-    }
+    check_args(query);
+    //match query {
+    //    Some(query) => println!("Query is {query}"),
+    //    None => panic!("minigrep requires a query"),
+    //}
 
     let file_path: Option<&String> = args.get(2);
-    match file_path {
-        Some(file_path) => println!("Filepath is {file_path}"),
-        None => panic!("minigrep requires a filepath"),
-    }
+    check_args(file_path);
+    //match file_path {
+    //    Some(file_path) => println!("Filepath is {file_path}"),
+    //    None => panic!("minigrep requires a filepath"),
+    //}
 
     dbg!(args);
 }
